0f7e7c0c399cf2359cf6a3b3cb51108e55e3ab63
yousrik20 <yousri.k.e13@gmail.com>
2025-06-29 12:41:31 +0200

    Lesson 8

---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink: "https://res.cloudinary.com/dbogfa6sk/image/upload/v1747679089/1_ws7kat.jpg",
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: "https://res.cloudinary.com/dbogfa6sk/image/upload/v1747679089/2_pwn2jl.jpg",
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink: "https://res.cloudinary.com/dbogfa6sk/image/upload/v1747679089/3_nf0wrj.jpg",
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: "https://res.cloudinary.com/dbogfa6sk/image/upload/v1747679089/4_qg9zm9.jpg",
+  },
+];
+module.exports = products;
