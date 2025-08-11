153e2f20f250703c28fabff2de2406237713a1b9
yousrik20 <yousri.k.e13@gmail.com>
2025-08-11 16:50:27 +0200

    Lesson 33

---
+++ frontend/src/pages/details/DetailsThumb.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DetailsThumb({myRef,images,tab}) {
+  return (
+    <div className="thumb" ref={myRef}>
+      {images.map((img, index) => (
+        <img src={img} alt="" key={index} onClick={() => tab(index)} />
+      ))}
+    </div>
+  );
+}
